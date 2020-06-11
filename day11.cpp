d16c8892b48e3d1e5441f886d9ff9077d807534e
Aditya Singh Goliya <44998673+aditya865@users.noreply.github.com>
2020-06-11 14:22:01 +0530

    Add files via upload

---
+++ day11.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+       int i, cnt0 = 0, cnt1 = 0, cnt2 = 0; 
+        int n=arr.size();
+    // Count the number of 0s, 1s and 2s in the array 
+    for (i = 0; i < n; i++) { 
+        switch (arr[i]) { 
+        case 0: 
+            cnt0++; 
+            break; 
+        case 1: 
+            cnt1++; 
+            break; 
+        case 2: 
+            cnt2++; 
+            break; 
+        } 
+    } 
+  
+    // Update the array 
+    i = 0; 
+  
+    // Store all the 0s in the beginning 
+    while (cnt0 > 0) { 
+        arr[i++] = 0; 
+        cnt0--; 
+    } 
+  
+    // Then all the 1s 
+    while (cnt1 > 0) { 
+        arr[i++] = 1; 
+        cnt1--; 
+    } 
+  
+    // Finally all the 2s 
+    while (cnt2 > 0) { 
+        arr[i++] = 2; 
+        cnt2--; 
+    } 
+    
+    }
+};
